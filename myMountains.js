e374ddb1e98a7e50a81d9188fa15a85d799eb01e
Phillip <phv9495@saintjoe.edu>
2015-02-27 00:39:39 -0500

    Add my own dataset for the mountains repo.

---
+++ myMountains.js
@@ -0,0 +1,17 @@
+//Phil Hofer
+//myMountains.js
+//2/26/15
+//Dataset for the mountains repo
+
+var MOUNTAINS = [
+  {name: "Kangchenjunga", height: 8586, country: "India"},
+  {name: "Lhotse", height: 8516, country: "China"},
+  {name: "Cho Oyu", height: 8201, country: "China"},
+  {name: "Elbrus", height: 5642, country: "russian Federation"},
+  {name: "Mount Kenya", height: 5199, country: "Kenya"},
+  {name: "Mount Logan", height: 5959, country: "Canada"},
+  {name: "Mawenzi", height: 5149, country: "Tanzania"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MOUNTAINS;
